ca62db5d7d17872fc109a2b2e8d54fffa8c5cd74
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-05 17:15:39 -0600

    Added controls div & color palette
    Added controls div under pad
    Added color palette with 16 colors to controls

--- scripts/sketchpad.js
+++ scripts/sketchpad.js
@@ -1,5 +1,11 @@
 const scechy = document.querySelector("#scechy");
 const scechyPad = scechy.querySelector("#pad");
+const padControls = scechy.querySelector("#padControls");
+const palette = padControls.querySelector("#palette");
+
+const colors = ["#000000", "#1D2B53", "#7E2553", "#008751", "#AB5236", 
+    "#5F574F", "#C2C3C7", "#FFF1E8", "#FF004D", "#FFA300", "#FFEC27",
+    "#00E436", "#29ADFF", "#83769C", "#FF77A8", "#FFCCAA"]
 
 function clearPad(){
     while(scechyPad.firstChild){
@@ -21,6 +27,22 @@ function fillPad(gridSize){
         scechyPad.appendChild(pix);
     }
 }
+function fillPalette(){
+    palette.replaceChildren();
+    const swatchSize = 100 / 16
+    colors.forEach((hexColor) => {
+        const swatch = document.createElement("div");
+        swatch.classList.add("swatch");
+        swatch.dataset.color = hexColor;
+        swatch.style.backgroundColor = hexColor;
+        swatch.style.width = swatchSize + "%";
+        swatch.style.height = swatchSize + "%";
+        palette.appendChild(swatch);
+    })
+}
+
+
+fillPalette();
 
 scechyPad.addEventListener("mouseover", (e) => {
     if(e.target.classList.contains("pixel")){
